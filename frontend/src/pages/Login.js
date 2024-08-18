9bec400dda63ebb341768efe89d65a305c57be06
sajitha-tj <sajithatj001@gmail.com>
2024-08-18 23:15:48 +0530

    login page ui

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
+import  heroImage from '../utils/images/landing_page_hero_image.svg';
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -18,23 +19,36 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className="w-full h-screen flex justify-center items-center bg-slate-200">
+      <div className="w-[60%] h-[70vh] rounded-xl overflow-hidden bg-slate-500 grid grid-cols-12 shadow-lg">
+        <div className="bg-white h-full px-10 py-10 col-span-7">
+          <img src={heroImage} alt="hero image" width={300} />
+          <h2 className="text-3xl font-bold">PosturePusle</h2>
+          <p className='text-xs uppercase tracking-wider'>AI physiotherapy assistant</p>
+        </div>
+        <div className=" col-span-5 px-8 relative">
+          <div className="absolute top-1/2 -translate-y-1/2 w-[81%]">
+            <h2 className="text-xl font-bold text-white mb-4 w-fit mx-auto">Login</h2>
+            <form onSubmit={handleLogin}>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full px-3 py-2 m-1 rounded-lg"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-3 py-2 m-1 rounded-lg"
+              />
+              <button type="submit" className="w-full px-3 py-2 m-1 rounded-lg bg-slate-700 text-white font-bold mt-3">login</button>
+            </form>
+          </div>
+          </div>
+      </div>
     </div>
   );
 }
